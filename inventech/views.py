5475a9c6b33aa5cf7b0a410d7a00d02ce11da05f
Juan Carlos Citelly Guzmán <juancaciguz@hotmail.com>
2024-08-09 20:53:27 -0500

    Filtración de palabara de busqueda searchLocation y parrametros units y products

--- inventech/views.py
+++ inventech/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product, ProductUnit
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    searchTerm = request.GET.get('searchLocation')
+    products = Product.objects.all()
+    if searchTerm:
+        units = ProductUnit.objects.filter(unit_location = searchTerm)
+    else:
+        units = ProductUnit.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'units': units, 'products': products})
+    #return render(request, 'home.html')
